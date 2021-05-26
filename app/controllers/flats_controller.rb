a0c51145acc89305d131f131f45f9cc589aee568
claire gautier <claire.gautier@edhec.com>
2021-05-26 09:43:11 +0200

    finished map set up

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -8,9 +8,11 @@ class FlatsController < ApplicationController
     @markers = @flats.geocoded.map do |flat|
       {
         lat: flat.latitude,
-        lng: flat.longitude
+        lng: flat.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { flat: flat })
       }
     end
+
   end
 
   # GET /flats/1
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,8 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -14,18 +17,22 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/clairegtr/ckhn654z00k4o19qqa2zkgln1'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.info_window); // add this
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup)
       .addTo(map);
     });
     fitMapToMarkers(map, markers);
-
-
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                      mapboxgl: mapboxgl }));
   }
 };
+
+
 
 export { initMapbox };
